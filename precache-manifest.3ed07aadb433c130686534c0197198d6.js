176b4252a42dc0a950f26f52e4099aef03696860
Jay Lowe <jaylowe@jays-mbp.lan>
2020-06-26 17:00:56 -0700

    Updates

---
+++ precache-manifest.3ed07aadb433c130686534c0197198d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9035c6003585c8d845c8a75a2fa509",
+    "url": "/school_tools_frontend_fresh/index.html"
+  },
+  {
+    "revision": "32596e7ee55710961c4d",
+    "url": "/school_tools_frontend_fresh/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "819b76497ba9e414b182",
+    "url": "/school_tools_frontend_fresh/static/css/main.635a0b59.chunk.css"
+  },
+  {
+    "revision": "32596e7ee55710961c4d",
+    "url": "/school_tools_frontend_fresh/static/js/2.0b13bbc1.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/school_tools_frontend_fresh/static/js/2.0b13bbc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819b76497ba9e414b182",
+    "url": "/school_tools_frontend_fresh/static/js/main.fadd7312.chunk.js"
+  },
+  {
+    "revision": "0780d65efa1a3333001c",
+    "url": "/school_tools_frontend_fresh/static/js/runtime-main.a1b33fb2.js"
+  }
+]);
